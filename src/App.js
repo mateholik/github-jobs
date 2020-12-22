7f37b9940f4c57703894b81bafd4ce8ab488c69d
mateholik <vaisov.vladimir@gmail.com>
2020-12-22 18:48:05 +0200

    fetch custom hook

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import React, { useState } from 'react'
+import useFetchJobs from './useFetchJobs';
+import { Container } from 'react-bootstrap'
 
 function App() {
+  const [params, setParams] = useState({})
+  const [page, setPage] = useState(1)
+  const { jobs, loading, error } = useFetchJobs(params, page)
   return (
-    <div></div>
+    <Container>
+      {loading && <h1>Loading...</h1>}
+      {error && <h1>Error. Try Refreshing.</h1>}
+      {jobs.map(job => {
+        return <Job key={job.id} job={job} />
+      })}
+    </Container>
   );
 }
 
