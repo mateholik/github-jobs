55639173a26bced329732191d067eeefa2ca9020
mateholik <vaisov.vladimir@gmail.com>
2020-12-23 15:21:59 +0200

    search

--- src/App.js
+++ src/App.js
@@ -2,14 +2,26 @@ import React, { useState } from 'react'
 import useFetchJobs from './Components/useFetchJobs'
 import Job from './Components/Job'
 import JobPagination from './Components/JobPagination'
+import Search from './Components/Search'
 import { Container } from 'react-bootstrap'
 
 function App() {
   const [params, setParams] = useState({})
   const [page, setPage] = useState(1)
   const { jobs, loading, error, hasNextPage } = useFetchJobs(params, page)
+
+  function handleParamChange(e) {
+    const param = e.target.name
+    const value = e.target.value
+    setPage(1)
+    setParams(prevParams => {
+      return { ...prevParams, [param]: value }
+    })
+  }
+
   return (
     <Container className="my-4">
+      <Search params={params} onParamChange={handleParamChange} />
       <h1 className="mb-4">GitHub Jobs</h1>
       <JobPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
       {loading && <h1>Loading...</h1>}
---
+++ src/Components/Search.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Form, Col } from 'react-bootstrap'
+
+function Search({ params, onParamChange }) {
+  return (
+    <Form className="mb-4">
+      <Form.Row className="align-items-end">
+        <Form.Group as={Col}>
+          <Form.Label>Description</Form.Label>
+          <Form.Control onChange={onParamChange} value={params.description || ''} name="description" type="text" placeholder="i.e senior, ruby, python" />
+        </Form.Group>
+        <Form.Group as={Col}>
+          <Form.Label>Location</Form.Label>
+          <Form.Control onChange={onParamChange} value={params.location || ''} name="location" type="text" placeholder="i.e London, California" />
+        </Form.Group>
+        <Form.Group as={Col} xs="auto" className="ml-2">
+          <Form.Check onChange={onParamChange} value={params.full_time} name="full_time" type="checkbox" id="full-time" label="Only Full Time" className="mb-2" />
+        </Form.Group>
+      </Form.Row>
+    </Form >
+
+  )
+}
+
+export default Search
